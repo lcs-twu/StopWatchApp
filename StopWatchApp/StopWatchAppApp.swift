0674b9289444dc677b26b95f1b68d6a35ca9fdca
Tom <twu@lcs.on.ca>
2022-11-09 09:08:52 -0500

    Initial Commit

---
+++ StopWatchApp/StopWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchAppApp.swift
+//  StopWatchApp
+//
+//  Created by Tom Wu on 2022-11-09.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
