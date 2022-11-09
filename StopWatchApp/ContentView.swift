013a9aeb9dcfaf65f2b61dc8bbd75332be9e9c7b
Tom <twu@lcs.on.ca>
2022-11-09 11:30:53 -0500

    tag

--- StopWatchApp/ContentView.swift
+++ StopWatchApp/ContentView.swift
@@ -44,24 +44,28 @@ struct ContentView_Previews: PreviewProvider {
                     Image(systemName: "globe")
                     Text("World Clock")
                 }
+                .tag(1)
             
             Text("Alarm")
                 .tabItem{
                     Image(systemName: "alarm.fill")
                     Text("Alarm Clock")
                 }
+                .tag(2)
             
             ContentView()
                 .tabItem{
                     Image(systemName: "stopwatch.fill")
                     Text("Stop Watch")
                 }
+                .tag(3)
             
             Text("Timer")
                 .tabItem{
                     Image(systemName: "timer")
                     Text("Timer")
                 }
+                .tag(4)
         }
         .accentColor(.orange)
         .preferredColorScheme(.dark)
